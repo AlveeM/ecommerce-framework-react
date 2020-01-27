8b976ee543ef58c9b680176ebbe7ebfff2104ae5
Alvee Akand <AlveeM@users.noreply.github.com>
2020-01-26 22:44:55 -0500

    added checkout page and added dispatch to cart-dropdown

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -19,3 +19,12 @@ export const selectCartItemsCount = createSelector(
             (acc, cartItem) => acc + cartItem.quantity, 0
         )
 )
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems =>
+        cartItems.reduce(
+            (acc, cartItem) => acc + cartItem.quantity * cartItem.price,
+            0
+        )
+)
