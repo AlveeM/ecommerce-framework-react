a70832d0ca26e29e9bab2c98833305a85a26ea21
Alvee Akand <AlveeM@users.noreply.github.com>
2020-01-27 16:11:04 -0500

    coverted directory data to object for faster lookup and refactored codebase

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,11 +7,20 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = () => (
+const CollectionPage = ({ collection }) => {
+    // console.log(collection);
+    const { title, items } = collection;
+
+    return (
     <div className='collection-page'>
-        <h2>COLLECTION PAGE</h2>
-    </div>
-)
+        <h2 className='title'>{ title }</h2>
+        <div className='items'>
+            {items.map(item => (
+                <CollectionItem key={item.id} item={item} />
+            ))}
+        </div>
+    </div>)
+}
 
 const mapStateToProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state)
--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -1,4 +1,5 @@
-const SHOP_DATA = [{
+const SHOP_DATA = {
+    hats: {
         id: 1,
         title: 'Hats',
         routeName: 'hats',
@@ -58,7 +59,7 @@ const SHOP_DATA = [{
             }
         ]
     },
-    {
+    sneakers: {
         id: 2,
         title: 'Sneakers',
         routeName: 'sneakers',
@@ -112,7 +113,7 @@ const SHOP_DATA = [{
             }
         ]
     },
-    {
+    jackets: {
         id: 3,
         title: 'Jackets',
         routeName: 'jackets',
@@ -148,10 +149,10 @@ const SHOP_DATA = [{
             }
         ]
     },
-    {
+    women: {
         id: 4,
-        title: 'Womens',
-        routeName: 'womens',
+        title: 'Women',
+        routeName: 'women',
         items: [{
                 id: 23,
                 name: 'Blue Tanktop',
@@ -196,10 +197,10 @@ const SHOP_DATA = [{
             }
         ]
     },
-    {
+    men: {
         id: 5,
-        title: 'Mens',
-        routeName: 'mens',
+        title: 'Men',
+        routeName: 'men',
         items: [{
                 id: 30,
                 name: 'Camo Down Vest',
@@ -238,6 +239,6 @@ const SHOP_DATA = [{
             }
         ]
     }
-];
+};
 
 export default SHOP_DATA;
