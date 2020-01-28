1cdfca84196f5e54b239c729d09f6831a2f5073b
Alvee Akand <AlveeM@users.noreply.github.com>
2020-01-28 15:37:07 -0500

    coverted collections-overview and collections styles to styled-components; added redux-thunk and refactored

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -5,20 +5,27 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
-import './collection.styles.scss';
+import {
+    CollectionPageContainer,
+    CollectionTitle,
+    CollectionItmesContainer
+} from './collection.styles';
+
+
 
 const CollectionPage = ({ collection }) => {
     const { title, items } = collection;
 
     return (
-    <div className='collection-page'>
-        <h2 className='title'>{ title }</h2>
-        <div className='items'>
-            {items.map(item => (
-                <CollectionItem key={item.id} item={item} />
-            ))}
-        </div>
-    </div>)
+        <CollectionPageContainer>
+            <CollectionTitle>{ title }</CollectionTitle>
+            <CollectionItmesContainer>
+                {items.map(item => (
+                    <CollectionItem key={item.id} item={item} />
+                ))}
+            </CollectionItmesContainer>
+        </CollectionPageContainer>
+    )
 }
 
 const mapStateToProps = (state, ownProps) => ({
